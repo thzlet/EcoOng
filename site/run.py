840e6359516636d5678c6dfbe58a09b46b32e4eb
leticiarosemberg <leticiarosembergsousa@gmail.com>
2021-10-07 15:08:50 +0000

    Update login.html

--- site/run.py
+++ site/run.py
@@ -2,7 +2,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 app.debug = True
 @app.route('/')
-@app.route('/home')
 
+@app.route('/home')
 def home_page():
     return render_template('home.html')
+
+@app.route('/login')
+def login_page():
+    return render_template('login.html')
