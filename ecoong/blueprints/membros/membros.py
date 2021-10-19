ce1233dedf0095b310f19f6ac0a4f79831a6d023
cristinaPRAT <cristinaaraujo3388@gmail.com>
2021-10-19 22:01:01 +0000

    Configuração das rotas e mockup de noticias e campanhas + arquivo detalhe_noticia.html

--- ecoong/blueprints/membros/membros.py
+++ ecoong/blueprints/membros/membros.py
@@ -2,17 +2,19 @@ from flask import Blueprint, render_template, request, redirect
 from ecoong.models import Membro
 from ecoong.ext.database import db
 
-bp = Blueprint('membros', __name__, template_folder='templates_mem', url_prefix='/membros')
+bp = Blueprint('membros', __name__,static_folder='static_mem', template_folder='templates_mem', url_prefix='/membros')
 
 
 @bp.route('/')
 def root():
     return 'Hello from membros'
 
+#rota de cadastro
 @bp.route('/cadastro')
 def cadastro_page():
     return render_template('membros/cadastro.html')
 
+#salvando os dados digitados no banco de dados
 @bp.post('/novocadastro')
 def cadastrar_page():
     membro = Membro()
@@ -27,26 +29,29 @@ def cadastrar_page():
 
     return redirect('/')
 
+#rota de login
 @bp.route('/login')
 def login_page():
     return render_template('membros/login.html')
 
+#validação de login
 @bp.post('/entrar')
 def form_login():
     verif_email = request.form['email']
     verif_senha = request.form['senha']
-    res=None
+
     membro = Membro.query.filter_by(email=verif_email).first()
 
     if membro:
         if verif_email == membro.email and verif_senha == membro.senha:
-            res=True
+            return 'parabensss'
         else:
-            res=False
-    if res == True:
-        return 'parabensss'
-    else:
-        return 'algo deu errado'
+            return 'algo deu errado'
 
+#atualizar dados
+
+#remover membro
+
+#registrando o blueprint membros
 def init_app(app):
     app.register_blueprint(bp)
--- ecoong/blueprints/noticias/noticias.py
+++ ecoong/blueprints/noticias/noticias.py
@@ -1,13 +1,24 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 
 
-bp = Blueprint('noticias', __name__, url_prefix='/noticias')
+bp = Blueprint('noticias', __name__, static_folder='static_not', template_folder='templates_not', url_prefix='/noticias')
 
 
-@bp.route('/')
-def root():
-    return 'Hello from noticias'
+noticia = [
+    {'id_noticia': 1,
+     'titulo': 'Lorem Ipsum',
+     'imagem': 'static_not/noticias/img/noticia.jpg',
+     'autor': 'Jubileu',
+     'data': '19/10/2021',
+     'descricao': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
+     'categoria': 'Lorem',
+     'tag': '#Ipsum',
+    }
+    ]
 
+@bp.route('/noticias')
+def noticias_page():
+    return render_template('noticias/noticia.html', noticias=noticia)
 
 def init_app(app):
     app.register_blueprint(bp)
