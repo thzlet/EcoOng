ae666160e1bd705d04093127c1e9509c6a5e1763
cristinaPRAT <cristinaaraujo3388@gmail.com>
2021-10-22 00:27:57 +0000

    modificação na tabela Membro

--- ecoong/models.py
+++ ecoong/models.py
@@ -3,7 +3,7 @@ from ecoong.ext.database import db
 class Membro(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     nome = db.Column(db.String(100), nullable=False)
-    email = db.Column(db.String(100), nullable=False)
-    senha = db.Column(db.String(100), nullable=False)
-    telefone = db.Column(db.Integer, default=0)
+    email = db.Column(db.String(50), nullable=False)
+    senha = db.Column(db.String(50), nullable=False)
+    telefone = db.Column(db.String(14), nullable=False)
     idade = db.Column(db.Integer, default=0)
