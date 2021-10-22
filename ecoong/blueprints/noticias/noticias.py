a43ddcc517452df1a494c619c8dd6f0402123acb
matheus monteiro <matheus.monteiro.vale07@aluno.ifce.edu.br>
2021-10-22 00:18:32 +0000

    Update blueprint noticias

--- ecoong/blueprints/noticias/noticias.py
+++ ecoong/blueprints/noticias/noticias.py
@@ -5,20 +5,18 @@ bp = Blueprint('noticias', __name__, static_folder='static_not', template_folder
 
 
 noticia = [
-    {'id_noticia': 1,
-     'titulo': 'Lorem Ipsum',
-     'imagem': 'static_not/noticias/img/noticia.jpg',
-     'autor': 'Jubileu',
-     'data': '19/10/2021',
-     'descricao': 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.',
-     'categoria': 'Lorem',
-     'tag': '#Ipsum',
-    }
-    ]
+    {'id_noticia': 1, 'titulo': 'Lorem Ipsum', 'imagem': 'static_not/noticias/img/noticia_onze.jpg', 'autor': 'Jubileu', 'data': '19/10/2021', 'descricao': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin suscipit elit.', 'categoria': 'Lorem', 'tag': '#Ipsum',},
+    {'id_noticia': 2, 'titulo': 'Lorem Ipsum', 'imagem': 'static_not/noticias/img/noticia_doze.jpg', 'autor': 'Jubileu', 'data': '19/10/2021', 'descricao': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin suscipit elit.', 'categoria': 'Lorem', 'tag': '#Ipsum',},
+    {'id_noticia': 3, 'titulo': 'Lorem Ipsum', 'imagem': 'static_not/noticias/img/noticia_treze.jpg', 'autor': 'Jubileu', 'data': '19/10/2021', 'descricao': 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sollicitudin suscipit elit.', 'categoria': 'Lorem', 'tag': '#Ipsum',},
+]
 
 @bp.route('/noticias')
 def noticias_page():
     return render_template('noticias/noticia.html', noticias=noticia)
+
+@bp.route('/detalhe_not')
+def detalhe_not_page():
+    return render_template('noticias/detalhe_noticia.html')
 
 def init_app(app):
     app.register_blueprint(bp)
