596bd3be9b34d0f3872974454a303a786eb00b85
cristinaPRAT <cristinaaraujo3388@gmail.com>
2021-12-11 21:36:44 +0000

    Campo de busca funcionando trazendo dados do banco, a busca funciona com titulo, descrição e tags

--- ecoong/__init__.py
+++ ecoong/__init__.py
@@ -1,11 +1,12 @@
 from flask import Flask
 from ecoong.ext import configuration
-from .views import root
+from .views import root, buscar_info
 
 def create_app():
     app = Flask(__name__)
     configuration.init_app(app)
 
     app.add_url_rule('/', view_func=root)
+    app.add_url_rule('/busca', view_func=buscar_info, methods=["POST"])
 
     return app
--- ecoong/views.py
+++ ecoong/views.py
@@ -1,9 +1,19 @@
-from flask import render_template
+from flask import render_template, request
 #from .models import Membro
 from ecoong.blueprints.campanhas.entidades import Campanha
-from ecoong.blueprints.noticias.entidades import Noticia
+from ecoong.blueprints.noticias.entidades import Noticia, Tag
+from sqlalchemy import or_
 
 def root():
     camp = Campanha.query.all()
     notc = Noticia.query.all()
     return render_template('index.html', campanhas = camp, noticias = notc)
+
+
+def buscar_info():
+    busca = request.form['busca']
+    search = '%{}%'.format(busca)
+    notc = Noticia.query.join(Noticia.tags).filter(or_(Noticia.titulo.like(search), Noticia.descricao.like(search), Tag.tag.like(search))).all()
+    cam = Campanha.query.filter(Campanha.titulo.like(search)).all()
+
+    return render_template('buscar_informacao.html', noticias = notc, campanhas = cam)
